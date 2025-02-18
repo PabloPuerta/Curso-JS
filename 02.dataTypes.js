20869e3cc47d14f3b6afa54627281ab009983bec
Pablo <pablopuertacasado@gmail.com>
2025-02-18 16:53:43 +0100

    primera clase

---
+++ 02.dataTypes.js
@@ -0,0 +1,23 @@
+// Cadena de texto (string)
+let name = "Pablo"
+let alias = "Lokotope"
+
+// Números (number)
+let edad = 37 // Entero
+let altura = 1.80 // Decimal
+
+// Booleanos (boolean)
+let estudiante = false
+let trabajador = true
+
+// Indefinida (undefined)
+let variableIndefinida
+
+// Nulos (null)
+let variableNull = null
+
+// Symbol
+let mySymbol = Symbol("mysymbol")
+
+// BigInt
+let myBigInt = BigInt(958685954784578451266584784654184151354549874513551894751654515468451)
---
+++ Hola-mundo/01-variables.js
@@ -0,0 +1,17 @@
+// var
+
+var holaMundo = "Hola mundo"
+console.log(holaMundo)
+
+holaMundo = "He cambiado el valor de la variable"
+console.log(holaMundo)
+
+// let
+
+let holaMundo2 = "Hola de nuevo mundo"
+console.log(holaMundo2)
+
+// conost
+
+const holaMundo3 = "Hola mundo 3"
+console.log(holaMundo3)
