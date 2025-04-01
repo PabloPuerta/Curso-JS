b466eeb77fc4d725262f746420ba0119845dac12
Pablo <pablopuertacasado@gmail.com>
2025-04-01 16:26:39 +0200

    01/04

---
+++ 14-bucles o loops.js
@@ -0,0 +1,21 @@
+// Bucles o Loops
+
+// for
+
+for (let i = 0; i < 5; i ++){
+    console.log("Hola " + i)
+}
+
+const numbers = [1, 2, 3, 4, 5]
+
+for(let x = 0; x < numbers.length; x++ ){
+    console.log(numbers[x])
+}
+
+// while
+let i = 0
+
+while (i < 5) {
+    console.log("hola")
+    i++
+}// 10. Crea un Array, transfórmalo a un Set y almacénalo en un Map
